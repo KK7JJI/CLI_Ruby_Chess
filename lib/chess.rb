cf67aa4ad80e589f0bc17754b59978781a02cece
kk7jji <kk7jji@outlook.com>
2026-04-16 15:42:39 -0700

    refactor BoardRank and BoardFile as subclasses
    These classes are now subclasses of
    BoardOffDiagonal.  Each needs to implement
    its own #calculate_initial_coordinates however
    all other code was identical.

--- lib/chess.rb
+++ lib/chess.rb
@@ -3,6 +3,7 @@
 require_relative 'chess/constants'
 require_relative 'chess/app'
 require_relative 'chess/render'
+require_relative 'chess/board/board_off_diagonal'
 require_relative 'chess/board/board_rank'
 require_relative 'chess/board/board_file'
 require_relative 'chess/board'
